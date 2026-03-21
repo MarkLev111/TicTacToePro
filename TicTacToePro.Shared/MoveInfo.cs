0c707b3e2f57dcd1cbbff5ded29a278e6ee3716a
Mark Levin (Laptop) <levinmark2008@gmail.com>
2026-03-21 22:17:02 +0300

    общий класс + зависимости от него

--- TicTacToePro.Shared/MoveInfo.cs
+++ TicTacToePro.Shared/MoveInfo.cs
@@ -2,6 +2,20 @@
 {
     public class MoveInfo
     {
-        priva
+        public int row { get; init; }
+        public int column { get; init; }
+        public char XOToPut { get; init; }
+        public int nextMove { get; init; }
+        public char result { get; init; }
+
+        public MoveInfo(int row, int column, char XOToPut, int nextMove, char result)
+        {
+            this.row = row;
+            this.column = column;
+            this.XOToPut = XOToPut;
+            this.nextMove = nextMove;
+            this.result = result;
+        }
+        public MoveInfo() { } // гемини сказал поставить конструктор без параметров
     }
 }

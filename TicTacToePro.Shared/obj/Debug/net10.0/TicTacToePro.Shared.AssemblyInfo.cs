31c65beb72ed8cc6c75d7f7ee9bd3403a4a9d08e
Mark Levin (Laptop) <levinmark2008@gmail.com>
2026-03-24 00:48:50 +0300

    изменил параметры логов

--- TicTacToePro.Shared/obj/Debug/net10.0/TicTacToePro.Shared.AssemblyInfo.cs
+++ TicTacToePro.Shared/obj/Debug/net10.0/TicTacToePro.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToePro.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5f0f6660846851324754d738d16999e48e17452")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fb77ea9d3b751796d84396116776fdafff45743")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToePro.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToePro.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

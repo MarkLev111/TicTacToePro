e51d8a650b810aaa4b3ac76b539010124ec023a4
Mark Levin (Laptop) <levinmark2008@gmail.com>
2026-04-11 16:11:38 +0300

    окошко статистики

--- TicTacToePro/StatsWindow.xaml.cs
+++ TicTacToePro/StatsWindow.xaml.cs
@@ -36,6 +36,28 @@ namespace TicTacToePro
             this.X.Text = $"Побед X: {Stats.currentStats?.Xwins} ({XwinPercent}%)";
             this.O.Text = $"Побед O: {Stats.currentStats?.Owins} ({OwinPercent}%)";
             this.Draw.Text = $"Ничейных игр: {Stats.currentStats?.draws} ({drawPercent}%)";
+
+            MultiplayerStats stats = Authorize.GetStats(this).Result;
+            if (!string.IsNullOrEmpty(stats.errorMessage))
+            {
+                this.Error.Text = stats.errorMessage;
+            }
+            else
+            {
+                this.MPGames.Text = $"Всего игр: {stats.games}";
+                int MPwinPercent = 0;
+                int MPlosePercent = 0;
+                int MPdrawPercent = 0;
+                if (stats.games != 0)
+                {
+                    MPwinPercent = stats.wins * 100 / stats.games;
+                    MPlosePercent = stats.loses * 100 / stats.games;
+                    MPdrawPercent = stats.draws * 100 / stats.games;
+                }
+                this.X.Text = $"Побед: {stats.wins} ({MPwinPercent}%)";
+                this.O.Text = $"Поражений {stats.loses} ({MPlosePercent}%)";
+                this.Draw.Text = $"Ничейных игр: {stats.draws} ({MPdrawPercent}%)";
+            }
         }
 
         private void Menu_Click(object sender, RoutedEventArgs e)
